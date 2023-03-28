2cda8a743f72c4e556ecc7240229236dc3174f3e
Rhenan Silverio Teixeira <rhenancontato@gmail.com>
2023-03-28 16:06:33 -0300

    criado components e chamada de api com fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1)
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(()=>{
+    (async function() {
+        let data = await fetch(api).then(res=>res.json())
+        console.log(data)
+    })()
+  }, [api])
   return (
     <div className="App">
      <h1 className="text-center ubuntu">
-      Rick and Morty -  <span className="text-primary">Teste Green acesso</span></h1>
+      Rick and Morty -  <span className="text-primary">Teste Green acesso</span>
+      </h1>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+          <Filters/>
+        </div>
+        <div className="col-8">
+          <div className="row" >
+            <div className="col-4">
+              <Cards/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     </div>
   );
 }
